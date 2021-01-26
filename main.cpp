c814359c244f1764cb8a4e6f06001f3344370dde
Chris Hartman <chris.m.hartman@gmail.com>
2021-01-26 11:07:28 -0900

    Demonstrate and use setName() and getName() in main() output.

--- main.cpp
+++ main.cpp
@@ -6,20 +6,29 @@ using std::vector;
 #include "Pet.hpp"
 
 void passPetByValue(Pet joe) {
-cout << "In passPetByValue.\n";
+   cout << "In passPetByValue.\n";
+   cout << "Pet's name is " << joe.getName() << "\n";
+   cout << "Changing name to Joe\n";
+   joe.setName("Joe");
+   cout << "Still in passPetByValue, pet's name is now " << joe.getName() << ".\n";
 }
 
 void passPetByReference(Pet &bill) {
    cout << "In passPetByReference.\n";
+   cout << "Pet's name is " << bill.getName() << "\n";
+   cout << "Changing name to Bill\n";
+   bill.setName("Bill");
 }
 
 void passPetByReferenceToConst(Pet const &bill) {
    cout << "In passPetByReferenceToConst.\n";
+   cout << "Pet's name is " << bill.getName() << "\n";
+   //bill.setName("Billy"); // This won't compile, can't change const object
 }
 
 Pet returnPetByValue() {
    cout << "In returnPetByValue.\n";
-   Pet amy;
+   Pet amy("Amy");
    return amy;
 }
 
@@ -33,12 +42,15 @@ int main() {
    Pet r("Tui");
    cout << "Pass an object by value:\n";
    passPetByValue(p);
+   cout << "After pass by value, name is " << p.getName() << ".\n";
    cout << "Pass an object by reference:\n";
    passPetByReference(p);
+   cout << "After pass by reference, name is " << p.getName() << ".\n";
    cout << "Pass an object by reference to const:\n";
    passPetByReferenceToConst(p);
    cout << "Return an object by value:\n";
-   returnPetByValue();
+   Pet a = returnPetByValue();
+   cout << "Returned object is named " << a.getName() << ".\n";
    cout << "Create a vector of 5 objects:\n";
    vector<Pet> v(5);
    return 0;
